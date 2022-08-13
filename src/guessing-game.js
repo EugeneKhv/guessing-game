84dce3688d017f8e6a5ff96d0147291e9833c7cc
Evgeniy Khvoynov <53225787+EugeneKhv@users.noreply.github.com>
2022-08-13 21:07:47 +0800

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.lowNum = 0;
+        this.uppNum = 0;
+        this.current = 0;
+    }
 
     setRange(min, max) {
-
+        this.lowNum = min;
+        this.uppNum = max;
     }
 
     guess() {
-
+        this.current = Math.round(this.lowNum + (this.uppNum - this.lowNum) / 2);
+        return this.current;
     }
 
     lower() {
-
+        this.uppNum = this.current;
     }
 
     greater() {
-
+        this.lowNum = this.current;
     }
 }
 
